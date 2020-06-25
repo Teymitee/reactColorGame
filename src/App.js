47b1d0ef0d1b98225d522fb2a83166025d94b7b9
Enikankiselu Temitope <ask4teymitee@gmail.com>
2020-06-25 08:28:34 +0100

    added play guide

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import NavBar from "./components/Navbar";
 import Container from "./components/Container";
 import Square from "./components/Square";
 import ModeButtons from "./components/ModeButtons";
-import './index.css'
+import "./index.css";
 
 class App extends Component {
   state = {
-      header: "#232323",
-      squares: [],
-      message: "",
-      modes: [
-        { name: "easy", numSquare: 3 },
-        { name: "intermediate", numSquare: 6 },
-        { name: "hard", numSquare: 9 },
-      ],
-      num: 3,
-      colors: [],
-      pickedColor: "",
-    };
+    header: "#232323",
+    squares: [],
+    message: "",
+    modes: [
+      { name: "easy", numSquare: 3 },
+      { name: "intermediate", numSquare: 6 },
+      { name: "hard", numSquare: 9 },
+    ],
+    num: 3,
+    colors: [],
+    pickedColor: "",
+  };
   componentDidMount() {
     this.generateRandomColors(this.state.num);
   }
@@ -49,11 +49,11 @@ class App extends Component {
       {
         num: num,
         message: "",
-        header: "#232323"
+        header: "#232323",
       },
       () => this.generateRandomColors(this.state.num)
     );
-  }
+  };
 
   handleCheck = (squareColor) => {
     let pickedColor = this.state.pickedColor;
@@ -69,25 +69,25 @@ class App extends Component {
           };
         });
       }
-    }else{
+    } else {
       this.setState(function () {
         return {
           message: "Wrong!",
         };
       });
     }
-  }
+  };
   reset = () => {
-    this.generateRandomColors(this.state.num)
+    this.generateRandomColors(this.state.num);
     this.setState({
-      message: ''
-    })
-  }
+      message: "",
+    });
+  };
   render() {
     let colors = this.state.colors,
       modes = this.state.modes,
       buttons = [],
-      squares = []
+      squares = [];
     for (let i = 0; i < colors.length; i++) {
       squares.push(
         <Square key={i} color={colors[i]} handleCheck={this.handleCheck} />
@@ -111,9 +111,28 @@ class App extends Component {
           headerColor={this.state.header}
         />
         <NavBar>
-          <button className="resetBtn" onClick={this.reset}>Reset</button>
+          <button className="resetBtn" onClick={this.reset}>
+            Reset
+          </button>
           <span>{this.state.message}</span> {buttons}
         </NavBar>
+        <div
+          style={{
+            backgroundColor: "white",
+            width: "300px",
+            padding: "15px",
+            margin: "50px 0px 0 20px",
+            float: "left",
+            borderRadius: "5px",
+          }}
+        >
+          <h2>How to Play</h2>
+          <p style={{ lineHeight: " 2" }}>
+            Choose a square which represents the rgb color code displayed in the
+            header. You can change level of difficulty by clicking "easy",
+            "Intermediate" or "Hard"
+          </p>
+        </div>
         <Container>{squares}</Container>
       </div>
     );
